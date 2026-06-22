ed7843deb844ee62b8ecffe22bee85b8f48e5d0a
Cauê <cauepbrito0@gmail.com>
2026-06-22 19:30:03 -0300

    Reusing instantiation and organizing Program

--- src/application/Program.java
+++ src/application/Program.java
@@ -11,8 +11,9 @@ public class Program {
     static void main(String[] args) {
 
         SellerDao sellerDao = DaoFactory.createSellerDao();
-
-        Seller seller = sellerDao.findById(3);
+        
+        System.out.println("==== TEST 1: seller findById ====");
+        Seller seller = sellerDao.findById(7);
 
         System.out.println(seller);
     }
