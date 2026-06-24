f842b9e947ca9d5e477e20dccd6f54e075bd6bbc
Cauê <cauepbrito0@gmail.com>
2026-06-24 11:23:20 -0300

    DepartmentDaoJDBC implementation and constructor in DaoFactory

---
+++ src/model/dao/impl/DepartmentDaoJDBC.java
@@ -0,0 +1,51 @@
+package model.dao.impl;
+
+import db.DB;
+import db.DbException;
+import model.dao.DepartmentDao;
+import model.entities.Department;
+import model.entities.Seller;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class DepartmentDaoJDBC implements DepartmentDao {
+
+    Connection conn;
+
+    public DepartmentDaoJDBC(Connection conn){
+        this.conn = conn;
+    }
+
+
+    @Override
+    public void insert(Department obj) {
+
+    }
+
+    @Override
+    public void update(Department obj) {
+
+    }
+
+    @Override
+    public void deleteById(Integer id) {
+
+    }
+
+    @Override
+    public Department findById(Integer id) {
+        return null;
+    }
+
+    @Override
+    public List<Department> findAll() {
+        return List.of();
+    }
+}
